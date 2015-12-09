d021473ab933875a5820c789d2c1ffccdbb2334e
mosluce <mosluce@gmail.com>
2015-12-10 00:10:54 +0800

    add spinkit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,40 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         uglify: {
-            options: {
-                banner: '/*! <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
             build: {
                 files: {
                     'js/build/ccmos-wizard.min.js': ['bower_components/jquery.steps/build/jquery.steps.js', 'js/src/ccmos-wizard.js'],
                     'js/build/ccmos-select2.min.js': ['bower_components/select2/dist/js/select2.full.js', 'js/src/ccmos-select2.js'],
                     'js/build/ccmos-datepicker.min.js': ['bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js', 'js/src/ccmos-datepicker.js'],
-                    'js/build/ccmos-checkbox.min.js': ['bower_components/iCheck/icheck.min.js', 'js/src/ccmos-checkbox.js']
+                    'js/build/ccmos-checkbox.min.js': ['bower_components/iCheck/icheck.min.js', 'js/src/ccmos-checkbox.js'],
+                    'js/build/ccmos-spinkit.min.js': ['js/src/ccmos-spinkit.js', 'js/src/ccmos-spinkit-templates.js']
+                }
+            }
+        },
+        ngtemplates: {
+            angularSpinkit: {
+                src: ['js/src/ccmos-spinkit-templates/**.html'],
+                dest: 'js/src/ccmos-spinkit-templates.js',
+                options: {
+                    module: 'ccmosSpinkit'
+                }
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    'css/ccmos-datepicker.css': 'scss/ccmos-datepicker.scss',
+                    'css/ccmos-select2.css': 'scss/ccmos-select2.scss',
+                    'css/ccmos-spinkit.css': 'scss/ccmos-spinkit.scss',
+                    'css/ccmos-wizard.css': 'scss/ccmos-wizard.scss'
                 }
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-angular-templates');
+    grunt.loadNpmTasks('grunt-sass');
 
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['ngtemplates', 'uglify', 'sass']);
 };
--- js/src/ccmos-spinkit.js
+++ js/src/ccmos-spinkit.js
@@ -4,47 +4,77 @@
             return {
                 restrict: 'E',
                 replace: true,
-                template: '<div class="sk-rotating-plane"></div>'
+                templateUrl: 'js/src/ccmos-spinkit-templates/sk-rotating-plane.html'
             }
         }])
         .directive('ccmosSkDoubleBounce', [function () {
             return {
                 restrict: 'E',
                 replace: true,
-                template: '' +
-                '<div class="sk-double-bounce">' +
-                '<div class="sk-child sk-double-bounce1"></div>' +
-                '<div class="sk-child sk-double-bounce2"></div>' +
-                '</div>'
+                templateUrl: 'js/src/ccmos-spinkit-templates/sk-double-bounce.html'
             }
         }])
         .directive('ccmosSkWave', [function () {
             return {
                 restrict: 'E',
-                //replace: true,
-                link: function(scope, element, attr) {
-                    var wave = $('<div class="sk-wave">');
-                    wave.append($('<div class="sk-rect sk-rect1"></div>'));
-                    wave.append($('<div class="sk-rect sk-rect2"></div>'));
-                    wave.append($('<div class="sk-rect sk-rect3"></div>'));
-                    wave.append($('<div class="sk-rect sk-rect4"></div>'));
-                    wave.append($('<div class="sk-rect sk-rect5"></div>'));
-
-                    element.append(wave);
-
-                    wave.unwrap();
-                }
+                replace: true,
+                templateUrl: 'js/src/ccmos-spinkit-templates/sk-wave.html'
             }
         }])
         .directive('ccmosSkWanderingCubes', [function () {
             return {
                 restrict: 'E',
                 replace: true,
-                template: '' +
-                '<div class="sk-wandering-cubes">' +
-                '<div class="sk-cube sk-cube1"></div>' +
-                '<div class="sk-cube sk-cube2"></div>' +
-                '</div>'
+                templateUrl: 'js/src/ccmos-spinkit-templates/sk-wandering-cubes.html'
+            }
+        }])
+        .directive('ccmosSkChasingDots', [function () {
+            return {
+                restrict: 'E',
+                replace: true,
+                templateUrl: 'js/src/ccmos-spinkit-templates/sk-chasing-dots.html'
+            }
+        }])
+        .directive('ccmosSkCircle', [function () {
+            return {
+                restrict: 'E',
+                replace: true,
+                templateUrl: 'js/src/ccmos-spinkit-templates/sk-circle.html'
+            }
+        }])
+        .directive('ccmosSkCubeGrid', [function () {
+            return {
+                restrict: 'E',
+                replace: true,
+                templateUrl: 'js/src/ccmos-spinkit-templates/sk-cube-grid.html'
+            }
+        }])
+        .directive('ccmosSkFadingCircle', [function () {
+            return {
+                restrict: 'E',
+                replace: true,
+                templateUrl: 'js/src/ccmos-spinkit-templates/sk-fading-circle.html'
+            }
+        }])
+        .directive('ccmosSkFoldingCube', [function () {
+            return {
+                restrict: 'E',
+                replace: true,
+                templateUrl: 'js/src/ccmos-spinkit-templates/sk-folding-cube.html'
+            }
+        }])
+        .directive('ccmosSkSpinnerPulse', [function () {
+            return {
+                restrict: 'E',
+                replace: true,
+                templateUrl: 'js/src/ccmos-spinkit-templates/sk-spinner-pulse.html'
+            }
+        }])
+        .directive('ccmosSkThreeBounce', [function () {
+            return {
+                restrict: 'E',
+                replace: true,
+                templateUrl: 'js/src/ccmos-spinkit-templates/sk-three-bounce.html'
             }
         }])
 })();
