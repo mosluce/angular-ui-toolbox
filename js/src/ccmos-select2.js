6f01bfa50be4d5fb5b03ff37a3f785372178b939
mosluce <mosluce@gmail.com>
2015-12-14 10:39:18 +0800

    select2 clear model when removed event

--- js/src/ccmos-select2.js
+++ js/src/ccmos-select2.js
@@ -3,10 +3,16 @@
         .directive('ccmosSelect2', ['$timeout', function ($timeout) {
             return {
                 restrict: 'A',
-                link: function (scope, element, attr) {
+                require: 'ngModel',
+                link: function (scope, element, attr, ngModel) {
                     $timeout(function () {
                         var settings = scope.$eval(attr.settings) || {};
                         element.select2(settings);
+                        element.on('select2-removed', function() {
+                            scope.$apply(function() {
+                                ngModel.$setViewValue(null);
+                            })
+                        });
                     });
                 }
             }
